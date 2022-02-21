d8ff1b64116292f089a6af92abf147dd316f1d3f
Pavel <slivnitsin1997@mail.ru>
2022-02-21 16:29:09 +0500

    Update camera.py

--- computer_vision/frame_acquisition/camera.py
+++ computer_vision/frame_acquisition/camera.py
@@ -23,10 +23,7 @@ class Camera:
         self.cam.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
         self.img_s = Queue(2)
-        self.result = Queue(1)
 
-    def is_face(self):
-        return self.result.full()
 
     def get_img(self):
         while self.img_s.empty():
@@ -40,22 +37,12 @@ class Camera:
 
             if not ret:
                 continue
-            img = cv2.flip(img, 0)
 
             if self.img_s.full():
                 self.img_s.get()
             self.img_s.put(img)
-
-            if not (res is None):
-                if self.result.empty():
-                    self.result.put(res)
-            else:
-                if self.result.full():
-                    self.result.get()
                 
             if debug:
-                if not (res is None):
-                    cv2.rectangle(img, (res[0], res[1]), (res[2], res[3]), (0, 255, 0), 2)
                 cv2.imshow('camera_output', img)
                 cv2.waitKey(1)
             
