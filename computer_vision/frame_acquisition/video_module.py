46c7d4923b9a6333fcd6c660103e571fe6ca013d
Pavel <slivnitsin1997@mail.ru>
2022-02-21 17:53:19 +0500

    vid

--- computer_vision/frame_acquisition/video_module.py
+++ computer_vision/frame_acquisition/video_module.py
@@ -4,16 +4,11 @@ import numpy as np
 import time
 
 
-class Camera():
-    def __init__(self, src=0, WIDTH=1280, HEIGHT=720, use_buffer=True):
-        self.src = src
-        self.WIDTH = WIDTH
-        self.HEIGHT = HEIGHT
+class Video():
+    def __init__(self, path, use_buffer=True):
+        self.path = path
         CODEC = cv2.VideoWriter_fourcc('M','J','P','G')
-
-        self.cam = cv2.VideoCapture(self.src)
-        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.WIDTH)
-        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.HEIGHT)
+        self.cam = cv2.VideoCapture(self.path)
         self.cam.set(cv2.CAP_PROP_FOURCC, CODEC)
         self.cam.set(cv2.CAP_PROP_FPS, 60)
         if use_buffer:
@@ -35,7 +30,8 @@ class Camera():
 
 if __name__ == '__main__':
     try:
-        c = Camera(0)
+        path = 'D:/exercise_tracker/Robot trainer video/IMG_0340.mp4'
+        c = Video(path)
         for img in c.get_img():
             cv2.imshow('img', img)
             if cv2.waitKey(1) == 27:
