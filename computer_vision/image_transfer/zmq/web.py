ca25645d3c9724e8966cb19e6d85a561e4e76d38
Alexandr <knxandr@rambler.ru>
2022-03-20 02:25:17 +0500

    zmq

--- computer_vision/image_transfer/zmq/web.py
+++ computer_vision/image_transfer/zmq/web.py
@@ -1,14 +1,14 @@
 import threading
 import time
-import zmq
+
 import numpy as np
 import cv2
-
-import pickle
 
 from flask import Response
 from flask import Flask
 from flask import render_template
+
+import zmq_module
 
 app = Flask(__name__)
 
@@ -23,17 +23,8 @@ def video_feed():
                     mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 def generate():
-  global socket
   while True:
-    try:
-      msg = socket.recv()
-    except Exception as e:
-      print('No connection')
-      time.sleep(0.1)
-      continue
-    socket.send_string('1', zmq.NOBLOCK)
-    
-    img = pickle.loads(msg)
+    img = rec.get_img()
     if img is None:
       time.sleep(0.1)
       continue
@@ -44,21 +35,9 @@ def generate():
     yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
             bytearray(encodedImage) + b'\r\n')
 
-def main_thread():
-  while True:
-    pass
-
 
 if __name__ == '__main__':
-  lock = threading.Lock()
-
-  img = None
-
-  ip = '0.0.0.0'
-
-  context = zmq.Context()
-  socket = context.socket(zmq.REP)
-  socket.RCVTIMEO = 1000
-  socket.connect("tcp://{}:{}".format(ip, 5001))
+  rec = zmq_module.ZMQ_receiver('192.168.68.205', 5005)
+  rec.run()
   
   app.run(host='0.0.0.0', port=58800, debug=False, threaded=True, use_reloader=False)
